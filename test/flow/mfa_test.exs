780040d8d3e8b953d67fe83c5f1615150add4610
Louis Matthijssen <l.matthijssen@yipyip.nl>
2023-04-18 11:04:11 +0200

    Clean up test

--- test/flow/mfa_test.exs
+++ test/flow/mfa_test.exs
@@ -98,27 +98,8 @@ defmodule MfaTest do
     test "error when trying to complete flow more than once" do
       user_id = Charon.Internal.Crypto.random_url_encoded(16)
 
-      assert %{
-               "stages" => [%{"key" => "password_stage"}, %{"key" => "totp_stage"}],
-               "token" => token
-             } = post_conn("/flows/mfa/start", %{"user_identifier" => user_id})
-
-      assert %{"result" => "completed"} =
-               post_conn(
-                 "/stages/password_stage/challenges/password/execute",
-                 %{"password" => "admin"},
-                 token
-               )
-
-      {:ok, %{totp_secret: secret}} = fetch_user(user_id)
-      totp_code = NimbleTOTP.verification_code(secret)
-
-      assert %{"result" => "completed"} =
-               post_conn(
-                 "/stages/totp_stage/challenges/totp/execute",
-                 %{"otp" => totp_code},
-                 token
-               )
+      assert %{"token" => token} =
+               post_conn("/flows/no_op/start", %{"user_identifier" => user_id})
 
       assert %{"challenge" => "complete"} = post_conn("/complete", %{}, token)
       assert %{"error" => "invalid_authorization"} = post_conn("/complete", %{}, token)
